45da94af688d8c53eab8dfa6609c7075c9143a65
axel COUDRAY <axel.hpm.coudray@gmail.com>
2019-10-22 22:01:05 +0200

    tp3 init

--- ServeurBibliotheque/src/modele/Bibliotheque.java
+++ ServeurBibliotheque/src/modele/Bibliotheque.java
@@ -2,13 +2,23 @@ package modele;
 
 import java.util.Optional;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import infrastructure.jaxrs.HyperLien;
 import infrastructure.jaxrs.HyperLiens;
 
 public interface Bibliotheque {
 		
+	@PUT
+	@Path("")
+	@Produces(MediaType.APPLICATION_XML)
 	Optional<HyperLien<Livre>> chercher(Livre l);
-
+	
+	@Path("catalogue")
 	HyperLiens<Livre> repertorier();
 	
 }
