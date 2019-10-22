a8e84bdd2fa7f6132f8ae6209b9158e1854f3fcd
axel COUDRAY <axel.hpm.coudray@gmail.com>
2019-10-22 14:32:37 +0200

    tp2 annotations

--- ServeurRegistre/src/infrastructure/Service.java
+++ ServeurRegistre/src/infrastructure/Service.java
@@ -1,11 +1,19 @@
 package infrastructure;
 
+import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import infrastructure.jaxrs.AjouterVersionAuxReponses;
+import infrastructure.jaxrs.Cacher;
+import infrastructure.jaxrs.CompterReponses;
+import infrastructure.jaxrs.CompterRequetes;
+import infrastructure.jaxrs.InteragirAtomiquement;
+import infrastructure.jaxrs.RealiserEcritureOptimiste;
 import modele.Registre;
 import modele.Ressource;
+import modele.Versionneur;
 
 
 
@@ -26,6 +34,25 @@ public class Service extends ResourceConfig {
 		Registre registre = new Registre(r);
 		this.register(registre);
 		
+		// Initialisation du décorateur avec version
+		Versionneur rV = new Versionneur(r);
+		// Enregistrement du lieur pour l'injection de dépendances relativement aux filtres
+		this.register(new AbstractBinder() {
+		  @Override
+		  protected void configure() {
+		    bind(rV)
+		      .to(Versionneur.class); 
+		  }
+		});
+		// Enregistrement des filtres 
+		//   (alternative possible : enregistrement automatique 
+		//		      si les filtres sont déclarés comme providers)
+		this.register(CompterRequetes.class);
+		this.register(CompterReponses.class);
+		this.register(new InteragirAtomiquement());
+		this.register(Cacher.class);
+		this.register(RealiserEcritureOptimiste.class);
+		this.register(AjouterVersionAuxReponses.class);
 	}
 }
 
--- ServeurRegistre/src/modele/ServiceRegistre.java
+++ ServeurRegistre/src/modele/ServiceRegistre.java
@@ -6,14 +6,34 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import infrastructure.jaxrs.annotations.AtomiciteRequeteReponseServeur;
+import infrastructure.jaxrs.annotations.CacheClient;
+import infrastructure.jaxrs.annotations.EcritureOptimiste;
+import infrastructure.jaxrs.annotations.StatReponses;
+import infrastructure.jaxrs.annotations.StatRequetes;
+import infrastructure.jaxrs.annotations.VersionReponses;
+
 public interface ServiceRegistre {
 	
 	@PUT
 	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
+	@AtomiciteRequeteReponseServeur
+	@CacheClient
+	@EcritureOptimiste
+	@StatReponses
+	@StatRequetes
+	@VersionReponses
 	public Ressource set(Ressource n);
 
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
+	@AtomiciteRequeteReponseServeur
+	@CacheClient
+	@EcritureOptimiste
+	@StatReponses
+	@StatRequetes
+	@VersionReponses
 	public Ressource get();
 }
 
